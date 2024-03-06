e8f677e683d3f7f22f0cd92c5b4f1cd2b3d4578f
Max de Visser <m.madv96@gmail.com>
2024-03-06 14:55:31 +0100

    updates to the type file

--- utils/project_types.py
+++ utils/project_types.py
@@ -2,8 +2,38 @@ from enum import Enum
 
 
 class TimeFrequency(Enum):
+    """A enum class to represent the different time frequencies for resampling the AIS dataset."""
     min_1 = "1min"
     min_10 = "10min"
     min_15 = "15min"
     min_30 = "30min"
     hour_1 = "1H"
+
+class ShipType(Enum):
+    """A enum class to represent the different ship types in the AIS dataset"""
+    sailboat = 'Sailing'
+    # 'Undefined'
+    # 'Cargo'
+    # 'Pleasure'
+    # 'Passenger'
+    # 'Fishing'
+    # 'Other',
+    # 'Pilot'
+    # 'Law enforcement'
+    # 'SAR'
+    # 'Military'
+    # 'Tug'
+    # 'Dredging'
+    # 'Towing'
+    # 'HSC'
+    # 'Tanker'
+    # 'Not party to conflict'
+    # 'Port tender'
+    # 'Reserved'
+    # 'WIG'
+    # 'Diving'
+    # 'Spare 1'
+    # 'Anti-pollution'
+    # 'Towing long/wide'
+    # 'Medical'
+    # 'Spare 2'
