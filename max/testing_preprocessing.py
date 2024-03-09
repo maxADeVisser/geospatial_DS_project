3ac7248d8768dd0e506c16f66e423a76cf2bfdd5
Max de Visser <max.devisser@zeronorth.com>
2024-03-09 13:21:58 +0100

    Merging

--- max/testing_preprocessing.py
+++ max/testing_preprocessing.py
@@ -10,7 +10,7 @@ from utils.project_types import TimeFrequency
 
 # %% Load the data
 # check in test/test_data/_test_data.py for how to generate the file used here
-aug1 = load_csv_file("data_files/test_aug_1_sailboat.csv")
+aug1 = load_csv_file("data_files/test_aug_1_sailboat.csv").drop(columns=["Ship type"])
 aug1 = remove_faulty_ais_readings(aug1)
 aug1
 
@@ -36,9 +36,9 @@ MMSI = aug1[aug1['MMSI'] == selected_MMSI]
 MMSI = to_geodf(MMSI)
 
 # change time frequency of data and plot
-plot_AIS_trace(change_data_frequency(MMSI, TimeFrequency.min_15))
+#plot_AIS_trace(change_data_frequency(MMSI, TimeFrequency.min_15))
 
 
-plot_static(MMSI)
+#plot_static(MMSI)
 
 # %%
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,7 +1,7 @@
 import datetime as dt
 import os
-import zipfile
 import subprocess
+import zipfile
 
 import geopandas as gpd
 import movingpandas as mpd
@@ -19,7 +19,7 @@ def download_ais_data(date: dt.date, out_folder: str) -> str:
     download_command = f"wget {url} -O {os.path.join(out_folder, file_name)}"
     # Use subprocess.run to execute the command and suppress output
     subprocess.run(download_command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    
+
     return os.path.join(out_folder, file_name)
 
 def unzip_ais_data(zip_file_path: str, out_folder: str) -> list:
@@ -52,54 +52,53 @@ def load_csv_file(filepath: str) -> pd.DataFrame:
     return df
 
 
+def remove_faulty_ais_readings(ais_df: pd.DataFrame) -> pd.DataFrame:
+    return ais_df.loc[(ais_df["lon"] != 0.0)]
+
+
 def change_data_frequency(
     ais_df: gpd.GeoDataFrame, data_freq: TimeFrequency
 ) -> gpd.GeoDataFrame:
     """Changes the data frequency of the dataframe.
     Resample every @data_freq and return the first value of each group."""
     crs = int(str(ais_df.crs).split(":")[-1])
-    ais_df = ais_df.resample(
-        rule=data_freq.value
-    ).first()
+    ais_df = ais_df.resample(rule=data_freq.value).first()
     ais_df.crs = crs
     return ais_df  # resample based on first index (timestamp)
 
 
-def to_geodf(ais_df: pd.DataFrame, projection: MapProjection = MapProjection.UTMzone32n) -> pd.DataFrame:
+def to_geodf(
+    ais_df: pd.DataFrame, projection: MapProjection = MapProjection.UTMzone32n
+) -> pd.DataFrame:
     """Turns a pd dataframe into geo pandas and project the coordinates
     into @projection (and drops non-used columns as of now)"""
     geo_ais_df = gpd.GeoDataFrame(
         ais_df, geometry=gpd.points_from_xy(ais_df.lon, ais_df.lat)
     )
-    geo_ais_df.crs = MapProjection.WGS84.value # original angular map projection
+    geo_ais_df.crs = MapProjection.WGS84.value  # original angular map projection
     epsg_code = int(projection.value.split(":")[-1])
     return geo_ais_df.to_crs(epsg=epsg_code).drop(columns=["lat", "lon", "Ship type"])
 
 
-def change_crs(geo_df: gpd.GeoDataFrame, crs: str) -> gpd.GeoDataFrame:
-    """Changes the coordinate reference system (CRS) of the GeoDataFrame."""
-    return geo_df.to_crs(crs)
+# def check_time_gap_threshold(
+#     ais_trace: list[int], time_gap_threshold: dt.timedelta
+# ) -> dt.timedelta | None:
+#     """Check that the AIS has no time gaps strictly larger than @time_gap_threshold"""
+#     assert all(
+#         ais_trace[i].datetime <= ais_trace[i + 1].datetime
+#         for i in range(len(ais_trace) - 1)
+#     ), "AIS trace is not in chronological order"
 
+#     for i in range(0, len(ais_trace)):
+#         if i == len(ais_trace) - 1:
+#             return None
+#         curr_time = ais_trace[i].datetime
+#         next_time = ais_trace[i + 1].datetime
+#         time_diff = next_time - curr_time
 
-def check_time_gap_threshold(
-    ais_trace: list[int], time_gap_threshold: dt.timedelta
-) -> dt.timedelta | None:
-    """Check that the AIS has no time gaps strictly larger than @time_gap_threshold"""
-    assert all(
-        ais_trace[i].datetime <= ais_trace[i + 1].datetime
-        for i in range(len(ais_trace) - 1)
-    ), "AIS trace is not in chronological order"
-
-    for i in range(0, len(ais_trace)):
-        if i == len(ais_trace) - 1:
-            return None
-        curr_time = ais_trace[i].datetime
-        next_time = ais_trace[i + 1].datetime
-        time_diff = next_time - curr_time
-
-        if time_diff.total_seconds() > time_gap_threshold.total_seconds():
-            return time_diff
-    return None
+#         if time_diff.total_seconds() > time_gap_threshold.total_seconds():
+#             return time_diff
+#     return None
 
 
 def reduce_data(csv_file_path: str, out_folder_path: str):
@@ -124,8 +123,36 @@ def reduce_data(csv_file_path: str, out_folder_path: str):
     print(f"Number of files created: {c}")
 
 
-def remove_faulty_ais_readings(ais_df: pd.DataFrame) -> pd.DataFrame:
-    return ais_df.loc[(ais_df["lon"] != 0.0)]
+def read_parquet(file_path: str, filter_MMSI: int | None = None) -> pd.DataFrame:
+    """Reads in a parquet file as a pandas dataframe.
+    If @filter_MMSI is provided, it only loads rows from that MMSI"""
+    if filter_MMSI:
+        filter = [("MMSI", "==", filter_MMSI)]
+
+    return pd.read_parquet(
+        path=file_path, filters=filter if filter_MMSI else None
+    )
+
+
+def extend_main_trajectory_file(
+    df: pd.DataFrame, file_name: str, main_file_path: str = "test/out"
+) -> None:
+    """If a main_trajectory file exists, it extends the main_trajectories files with the ones provided in @df.
+    If the file does not exists, create it."""
+
+    if not os.path.exists(main_file_path):
+        os.mkdir(main_file_path)
+
+    # TODO if the main file does not exists yet, then create an empty one:
+    # if not os.path.exists(main_file_path + "/main_trajectories.parquet"):
+
+
+    df.to_parquet(
+        path=file_path + file_name,
+    )
+
+
+
 
 
 if __name__ == "__main__":
