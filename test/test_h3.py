5f0874325ff24aa3801d8ab52b6d67bc29f356b0
max-zeronorth <max.devisser@zeronorth.com>
2024-04-17 19:04:35 +0200

    played around with h3

--- test/test_h3.py
+++ test/test_h3.py
@@ -1,3 +1,4 @@
+# %%
 import geopandas as gpd
 import h3
 import matplotlib.pyplot as plt
@@ -6,6 +7,7 @@ from shapely.geometry import Polygon
 
 from utils.plotting import plot_static
 from utils.preprocessing import to_geodf
+from utils.project_types import MapProjection
 
 # code is from: https://github.com/uber/h3-py-notebooks/blob/master/notebooks/unified_data_layers.ipynb
 
@@ -58,29 +60,31 @@ def kring_smoothing(df, hex_col, metric_col, k):
     return dfs
 
 
-df = pd.read_parquet("out/ais_data/fishing_first_10_days.parquet")
-# gdf = to_geodf(df)
-# plot_static(gdf, alpha=0.01)
-
-H3_RESOLUTION = 9
+df = pd.read_parquet("out/ais_data/traj_first_10_days.parquet")
+H3_RESOLUTION = 7
 hex_col = "h3_hex" + str(H3_RESOLUTION)
-
-# lat_lng_to_h3 = lambda row, resolution=H3_RESOLUTION: h3.geo_to_h3(
-#     row["geometry"].x, row["geometry"].y, resolution
-# )
-# gdf[hex_col] = gdf.apply(lat_lng_to_h3, axis=1)
-
 
 # find hexs containing the points
 df[hex_col] = df.apply(lambda x: h3.geo_to_h3(x.lat, x.lon, H3_RESOLUTION), axis=1)
 
 # aggregate the points
-# dfg = df.groupby(hex_col).size().to_frame("counts").reset_index()
-hex_counts = df.groupby([hex_col])[hex_col].agg("count").to_frame("count").reset_index()
+# this does the same things as below
+hex_counts = df[hex_col].value_counts().to_frame("counts").reset_index()
+# hex_counts = (
+#     df.groupby([hex_col])[hex_col]
+#     .agg("counts")
+#     .to_frame("count")
+#     .reset_index()
+# )
+
+# filter out hexes with less than X AIS counts in it
+min_readings = 100
+# max_readings = 10000 # TODO add a max reading or do kring smoothing
+hex_counts = hex_counts[hex_counts["counts"] > min_readings]
 
 # find center of hex for visualisation
-# hex_counts["lat"] = hex_counts[hex_col].apply(lambda x: h3.h3_to_geo(x)[0])
-# hex_counts["lon"] = hex_counts[hex_col].apply(lambda x: h3.h3_to_geo(x)[1])
+hex_counts["lon"] = hex_counts[hex_col].apply(lambda x: h3.h3_to_geo(x)[1])
+hex_counts["lat"] = hex_counts[hex_col].apply(lambda x: h3.h3_to_geo(x)[0])
 
 
 def add_geometry(row):
@@ -91,8 +95,15 @@ def add_geometry(row):
 # Create the actual hexagon geometries:
 hex_counts["geometry"] = hex_counts.apply(add_geometry, axis=1)
 
-hex_counts = to_geodf(hex_counts)
+hex_counts = gpd.GeoDataFrame(hex_counts, geometry="geometry")
 
+hex_counts.crs = MapProjection.WGS84.value  # original angular map projection
+epsg_code = int(MapProjection.UTMzone32n.value.split(":")[-1])
+hex_counts = hex_counts.to_crs(epsg=epsg_code)
+
+# %%
+
+# hex_counts.explore()
 
 # plot the hexs
 plot_scatter(hex_counts, metric_col="counts", marker="o", figsize=(17, 15))
@@ -100,8 +111,23 @@ plt.title("hex-grid: noise complaints")
 
 
 # kring_smoothing
-# k = 2
-# df311s = kring_smoothing(hex_counts, hex_col, metric_col="counts", k=k)
-# print("sum sanity check:", df311s["counts"].sum() / hex_counts["counts"].sum())
-# plot_scatter(df311s, metric_col="counts", marker="o")
-# plt.title("noise complaints: 2-ring average")
+k = 2
+df311s = kring_smoothing(hex_counts, hex_col, metric_col="counts", k=k)
+print("sum sanity check:", df311s["counts"].sum() / hex_counts["counts"].sum())
+plot_scatter(df311s, metric_col="counts", marker="o")
+
+df311s.plot.scatter(
+    x="lon",
+    y="lat",
+    c="counts",
+    title="counts",
+    edgecolors="none",
+    colormap="viridis",
+    marker="o",
+    alpha=1,
+    figsize=(16, 12),
+)
+plt.xticks([], [])
+plt.yticks([], [])
+plt.title("noise complaints: 2-ring average")
+plt.show()
